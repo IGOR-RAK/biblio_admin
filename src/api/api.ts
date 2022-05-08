d714ef6baf119ce256a345f2af1b2045f50aa90a
Igor Rak <igorrakfed@gmail.com>
2022-05-08 19:57:07 +0200

    api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 export const API = {
-  //  PROD:"https://salty-springs-71498.herokuapp.com",
-  LOCAL: "http://localhost:5000",
-  PROD: "",
+    PROD:"https://salty-springs-71498.herokuapp.com",
+  // LOCAL: "http://localhost:5000",
+  // PROD: "",
 };
 
 export const ROUTES = {
